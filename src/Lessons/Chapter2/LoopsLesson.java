f73d552f470e1d2a618ade46abedb4c9d670d389
Masamune-dxd <aetulabing@gmail.com>
2025-07-21 09:16:56 +0800

    transfer files

---
+++ src/Lessons/Chapter2/LoopsLesson.java
@@ -0,0 +1,14 @@
+package Lessons.Chapter2;
+
+public class LoopsLesson { 
+    
+    public static void main(String[] args) {
+        byte a = 1;
+        while(a<=10)
+            System.out.println("a = "+a++);
+
+        do {
+            System.out.println("a = "+a--);
+        } while(a>0);
+    }
+}
